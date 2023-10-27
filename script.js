56ef6a3775a5c92b682e19d028ee7408c70b25df
Harmony Burke <theharmonyburke@gmail.com>
2023-10-26 19:25:03 -0500

    added function for local storage of the album information with empty spots to fill in once HTML is completed

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ fetch (artist)
     return data;
 })
 
+
+localStorage.setItem(artist, album)
+// this should go within the search event listener so that it saves the information on page load
+$(".album").each(function(){
+    var saveAlbum=$(this)
+    // looks for current album and saves it 
+    var albumName=localStorage.getItem(saveAlbum);
+    // gets the album saved to local storage
+
+    if (albumName){
+        $(this).find("enterhtml").val(albumName)
+        // finds the album name and saves it to the html class/id
+    }
+})
