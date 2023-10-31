be47d1ba76d2d76a500d198f43e518f7e39a1d7b
Harmony Burke <theharmonyburke@gmail.com>
2023-10-30 21:33:40 -0500

    add API for album and working on fixing bugs in js

--- script.js
+++ script.js
@@ -1,48 +1,81 @@
-var artist = `http://theaudiodb.com/api/v1/json/2/album.php?i=112024`;
-console.log(artist);
 
 
-fetch(artist)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    return data;
-  });
-
+var artistSearch = $("#artists-input")
+var searchBtn = $("#search-button")
+var randomDiv = $("#random-album")
 
 
 
 
 document.addEventListener("DOMContentLoaded", function () {
-  const urlParams = new URLSearchParams(window.location.search);
-  const artistInput = urlParams.get("artist");
 
-  if (artistInput) {
-    // document.getElementById("artists-input").value = artistInput;
+  searchBtn.on("click", function () {
+    // event.preventDefault()
+    var artistName = artistSearch.val()
+      window.location.href = `page2.html?artist=${encodeURIComponent(artistName)}`;
+      // window.location.href=`page2.html`
+      getAlbum(artistName)
+  })
 
-    performSearch(artistInput);
+  function getAlbum(artistName) {
+    console.log(artistName)
+    var artistIdApi = `http://theaudiodb.com/api/v1/json/2/discography.php?s=` + artistName.toLowerCase()
+
+    fetch(artistIdApi)
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        console.log(data)
+
+        var albums = data.album;
+        console.log(albums)
+
+        // Check if there are albums in the response
+        if (albums.length > 0) {
+          // Generate a random index within the valid range
+          var randomIndex = Math.floor(Math.random() * albums.length);
+
+          // Get the random album
+          var randomAlbum = albums[randomIndex].strAlbum;
+
+          console.log('Random Album:', randomAlbum);
+          randomDiv.textContent=randomAlbum
+          randomDiv.append(randomAlbum)
+
+        } else {
+
+          console.log('No albums found for the artist');
+        }
+
+
+      })
+
+
+
+
+
   }
-
-  document
-    .getElementById("search-button")
-    .addEventListener("click", function () {
-      const artistInput = document.getElementById("artists-input").value;
-
-      window.location.href = `page2.html?artist=${encodeURIComponent(
-        artistInput
-      )}`;
-    });
-
- 
-function performSearch(artistInput) {
-    
-    const searchQuery = `${artistInput} + _(musician)`;
-    // const apiUrl = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchQuery}&format=json&origin=*`;
-    // const section = "2"; // Specify the section you want
-    const apiUrl = `https://en.wikipedia.org/w/api.php?action=parse&page=${searchQuery}&format=json&origin=*`;
   
+  
+  
+  
+ 
+    const urlParams = new URLSearchParams(window.location.search);
+    const artistInput = urlParams.get("artist");
+    if (artistInput) {
+      // document.getElementById("artists-input").value = artistInput;
+      performSearch(artistInput);
+    }
+    document
+      .getElementById("search-button")
+     
+  
+  
+  
+  function performSearch(artistInput) {
+    const searchQuery = `${artistInput} + _(musician)`;
+    const apiUrl = `https://en.wikipedia.org/w/api.php?action=parse&page=${searchQuery}&format=json&origin=*`;
     fetch(apiUrl)
       .then((response) => {
         if (!response.ok) {
@@ -51,53 +84,51 @@ function performSearch(artistInput) {
         return response.json();
       })
       .then((data) => {
-       
-        displaySearchResults(data);
+        displaySearchResults(data, artistInput);
         console.log(data);
-        console.log(data.parse.sections[1]);
-        document.getElementById("wikibio").innerHTML = data.parse.anchors[0];
       })
       .catch((error) => {
         console.error("Fetch error:", error);
       });
   }
-  
 
-  function displaySearchResults(data) {
+
+
+  function displaySearchResults(data, artistInput) {
     const wikiBioElement = document.getElementById("wikibio");
-  
-    // Clear previous search results
     wikiBioElement.innerHTML = "";
-  
-    // Extract and display the artist biography
-    const searchTerm = data[0];
-    const searchBiography = data[2][0]; // The artist biography is in the first entry of the data[2] array
-  
-    // Set the content of the "wikibio" element
-    wikiBioElement.innerHTML = `<strong>${searchTerm}</strong>: ${searchBiography}`;
+    const searchTerm = artistInput;
+    const wikiUrl = `https://en.wikipedia.org/wiki/${searchTerm.replace(
+      / /g,
+      "_"
+    )}`;
+    wikiBioElement.innerHTML = `<strong>${searchTerm}</strong>: <a href="${wikiUrl}" target="_blank">Wikipedia Link</a>`;
   }
-});
-$("#saved-albums").each(function(){
-  // looks for current album and saves it 
-  var albumName=localStorage.getItem(saveAlbum);
-  // gets the album saved to local storage
-  var saveAlbum=albumName.val()
-  // this pulls the value from the entered information
 
-  if (albumName){
-      $(this).find(saveAlbum).val(albumName)
-      // finds the album name and saves it to the html class/id
+
+$("#saved-albums").each(function () {
+  // looks for current album and saves it 
+  var albumName = localStorage.getItem(saveAlbum);
+  // gets the album saved to local storage
+  var saveAlbum = albumName.val()
+  // this pulls the value from the entered information
+  
+  if (albumName) {
+    $(this).find(saveAlbum).val(albumName)
+    // finds the album name and saves it to the html class/id
   }
 
 })
-$("#artist-info").each(function(){
-  var artistWiki=$("#artist-info").val()
+$("#artist-info").each(function () {
+  var artistWiki = $("#artist-info").val()
   // this pulls the value entered into the input field 
-  var saveWiki=localStorage.getItem(artistWiki)
+  var saveWiki = localStorage.getItem(artistWiki)
   // this gets the information from the wiki that has been set to local storage
-  if(saveWiki){
-      $(this).find(saveWiki).val(artistWiki)
-      // find the info and sets it
+  if (saveWiki) {
+    $(this).find(saveWiki).val(artistWiki)
+    // find the info and sets it
   }
-
+  
+  
+})
 })
