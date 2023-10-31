c2545faeb1b5f21cac834f3d7124bdc48c8d0c5b
Harmony Burke <theharmonyburke@gmail.com>
2023-10-31 18:35:11 -0500

    need to de bug the get album function not pulling on page load

--- script.js
+++ script.js
@@ -1,118 +1,124 @@
 
 
-var artistSearch = $("#artists-input")
-var searchBtn = $("#search-button")
-var randomDiv = $("#random-album")
+$(document).ready(function () {
 
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
+   var artistSearch = $("#artists-input")
+  var searchBtn = $("#search-button")
+  var randomDiv = $("#random-album")
 
   searchBtn.on("click", function () {
     // event.preventDefault()
+    console.log("hello")
     var artistName = artistSearch.val()
-      window.location.href = `page2.html?artist=${encodeURIComponent(artistName)}`;
-      // window.location.href=`page2.html`
-      getAlbum(artistName)
+    getAlbum(artistName)
+    
+    window.location.href = `page2.html?artist=${encodeURIComponent(artistName)}`;
+    // window.location.href=`page2.html`
   })
 
   function getAlbum(artistName) {
+    var artistName = artistSearch.val()
     console.log(artistName)
+    
     var artistIdApi = `http://theaudiodb.com/api/v1/json/2/discography.php?s=` + artistName.toLowerCase()
-
+  
     fetch(artistIdApi)
       .then(function (response) {
         return response.json()
       })
       .then(function (data) {
         console.log(data)
-
+  
         var albums = data.album;
         console.log(albums)
-
+  
         // Check if there are albums in the response
-        if (albums.length > 0) {
+        if (albums && albums.length > 0) {
           // Generate a random index within the valid range
           var randomIndex = Math.floor(Math.random() * albums.length);
-
+  
           // Get the random album
           var randomAlbum = albums[randomIndex].strAlbum;
-
+  
           console.log('Random Album:', randomAlbum);
-          randomDiv.textContent=randomAlbum
-          randomDiv.append(randomAlbum)
-
+          // randomDiv.textContent=randomAlbum
+          randomDiv.empty().append(randomAlbum);
+  
         } else {
-
+  
           console.log('No albums found for the artist');
         }
-
-
+  
+  
       })
-
-
-
-
-
-  }
-  
-  
-  
-  
- 
-    const urlParams = new URLSearchParams(window.location.search);
-    const artistInput = urlParams.get("artist");
-    if (artistInput) {
-      // document.getElementById("artists-input").value = artistInput;
-      performSearch(artistInput);
-    }
-    document
-      .getElementById("search-button")
-     
-  
-  
-  
-  function performSearch(artistInput) {
-    const searchQuery = `${artistInput} + _(musician)`;
-    const apiUrl = `https://en.wikipedia.org/w/api.php?action=parse&page=${searchQuery}&format=json&origin=*`;
-    fetch(apiUrl)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        displaySearchResults(data, artistInput);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
+      .catch(function (error) {
+        console.error('Error fetching data:', error);
       });
+  
+  
+  
+  
+  
   }
 
 
 
-  function displaySearchResults(data, artistInput) {
-    const wikiBioElement = document.getElementById("wikibio");
-    wikiBioElement.innerHTML = "";
-    const searchTerm = artistInput;
-    const wikiUrl = `https://en.wikipedia.org/wiki/${searchTerm.replace(
-      / /g,
-      "_"
-    )}`;
-    wikiBioElement.innerHTML = `<strong>${searchTerm}</strong>: <a href="${wikiUrl}" target="_blank">Wikipedia Link</a>`;
-  }
 
 
+
+
+const urlParams = new URLSearchParams(window.location.search);
+const artistInput = urlParams.get("artist");
+if (artistInput) {
+  // document.getElementById("artists-input").value = artistInput;
+  performSearch(artistInput);
+}
+document
+  .getElementById("search-button")
+
+
+
+
+function performSearch(artistInput) {
+  const searchQuery = `${artistInput} + _(musician)`;
+  const apiUrl = `https://en.wikipedia.org/w/api.php?action=parse&page=${searchQuery}&format=json&origin=*`;
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      displaySearchResults(data, artistInput);
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+}
+
+
+
+function displaySearchResults(data, artistInput) {
+  const wikiBioElement = document.getElementById("wikibio");
+  wikiBioElement.innerHTML = "";
+  const searchTerm = artistInput;
+  const wikiUrl = `https://en.wikipedia.org/wiki/${searchTerm.replace(
+    / /g,
+    "_"
+  )}`;
+  wikiBioElement.innerHTML = `<strong>${searchTerm}</strong>: <a href="${wikiUrl}" target="_blank">Wikipedia Link</a>`;
+}
+
+})
 $("#saved-albums").each(function () {
   // looks for current album and saves it 
   var albumName = localStorage.getItem(saveAlbum);
   // gets the album saved to local storage
   var saveAlbum = albumName.val()
   // this pulls the value from the entered information
-  
+
   if (albumName) {
     $(this).find(saveAlbum).val(albumName)
     // finds the album name and saves it to the html class/id
@@ -128,7 +134,7 @@ $("#artist-info").each(function () {
     $(this).find(saveWiki).val(artistWiki)
     // find the info and sets it
   }
-  
-  
+
+
 })
-})
+
